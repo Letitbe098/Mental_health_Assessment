849ae71fc86b4bc26dbee8652f7c6970232cc4b8
Letitbe098 <banothpoojitha15@gmail.com>
2025-05-30 00:07:25 +0530

    Deploy application to Netlify

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,14 +4,15 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBHieLdsjcZRXXNogO38B-E9D8TE3KRv5U",
+  authDomain: "mental-health-assessment-new.firebaseapp.com",
+  projectId: "mental-health-assessment-new",
+  storageBucket: "mental-health-assessment-new.firebasestorage.app",
+  messagingSenderId: "824298955850",
+  appId: "1:824298955850:web:01fa24865008e10768b2b3",
+  measurementId: "G-PQVS59KTSD"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
